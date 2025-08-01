29101ed184d4e98cbaade4c0125348bdc7d0bf74
sansds35-collab <sansds35@gmail.com>
2025-08-01 17:52:56 +0800

    Add files via upload

---
+++ 魔芋爽.lua
@@ -0,0 +1,16 @@
+local OrionLib = 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wsomoQaz/lua-/main/Xcccc"))()
+
+local Window = OrionLib:MakeWindow({
+    IntroText = "缝合脚本", 
+    Name = "缝合脚本 " .. identifyexecutor(), 
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = ""
+})
+
+local Tab = Window:MakeTab({
+    Name = "功能",
+    Icon = "rbxassetid://7733779610",
+    PremiumOnly = false
+})
