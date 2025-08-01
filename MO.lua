b2c1b34928ffea5b02f0e8168e8f770db4a47dcf
sansds35-collab <sansds35@gmail.com>
2025-08-01 18:02:47 +0800

    Add files via upload

---
+++ MO.lua
@@ -0,0 +1,24 @@
+local OrionLib = 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wsomoQaz/lua-/main/Xcccc"))()
+
+local Window = OrionLib:MakeWindow({
+    IntroText = "MO", 
+    Name = "MO" .. identifyexecutor(), 
+    HidePremium = false,
+    SaveConfig = true,
+    ConfigFolder = ""
+})
+
+local Tab = Window:MakeTab({
+    Name = "CDID",
+    Icon = "rbxassetid://7733779610",
+    PremiumOnly = false
+})
+
+Tab:AddButton({
+    Name = "CDID NE"
+    Callback = function()
+    _G.Script_Key = "MoonKey_CdidByNEHubUserninth"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LENG8123/cdid-script/refs/heads/main/cdid.txt"))()
+    end
+})
